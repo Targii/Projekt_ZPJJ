259da997e507759c7803bddf8064c200df4a88cd
maciejparol <parol.m@o2.pl>
2017-01-27 00:52:43 +0100

    mala poprawka w frontendzie

--- src/main/webapp/assets/js/changecontent.js
+++ src/main/webapp/assets/js/changecontent.js
@@ -1,20 +1,20 @@
 function load_home() {
-     document.getElementById("service").innerHTML = "";
-     document.getElementById("service").innerHTML='<object type="text/html" data="home.html" ></object>';
+    document.getElementById("service").innerHTML = "";
+    document.getElementById("service").innerHTML = '<object type="text/html" data="home.html" style="width:100%; height: 100%;"></object>';
 }
 function load_add() {
-     document.getElementById("service").innerHTML = "";
-     document.getElementById("service").innerHTML='<object type="text/html" data="add.html" ></object>';
+    document.getElementById("service").innerHTML = "";
+    document.getElementById("service").innerHTML = '<object type="text/html" data="add.html" style="width:100%; height: 100%;"></object>';
 }
 function load_manage() {
-     document.getElementById("service").innerHTML = "";
-     document.getElementById("service").innerHTML='<object type="text/html" data="manage.html" ></object>';
+    document.getElementById("service").innerHTML = "";
+    document.getElementById("service").innerHTML = '<object type="text/html" data="manage.html" style="width:100%; height: 100%;"></object>';
 }
 function load_search() {
-     document.getElementById("service").innerHTML = "";
-     document.getElementById("service").innerHTML='<object type="text/html" data="search.html" ></object>';
+    document.getElementById("service").innerHTML = "";
+    document.getElementById("service").innerHTML = '<object type="text/html" data="search.html" style="width:100%; height: 100%;"></object>';
 }
 function load_remove() {
-     document.getElementById("service").innerHTML = "";
-     document.getElementById("service").innerHTML='<object type="text/html" data="remove.html" ></object>';
+    document.getElementById("service").innerHTML = "";
+    document.getElementById("service").innerHTML = '<object type="text/html" data="remove.html" style="width:100%; height: 100%;"></object>';
 }
